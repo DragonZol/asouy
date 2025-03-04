412524bcdcd955d408f1a3d7b1337d48926ae404
dandy <dandyincorp1@gmail.com>
2025-03-04 19:38:59 +0800

    Создал заготовку

---
+++ src/main/java/com/example/project_assouy/ProjectAssouyApplication.java
@@ -0,0 +1,13 @@
+package com.example.project_assouy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectAssouyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectAssouyApplication.class, args);
+    }
+
+}
